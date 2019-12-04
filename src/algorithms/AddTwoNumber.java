b1189cf8b199ccfb1c6dfe17b34074e93f4a98d9
lujin <lujin.vip@foxmail.com>
2019-12-04 09:29:05 +0800

    两数相加, 无重复字符的最长子串

---
+++ src/algorithms/AddTwoNumber.java
@@ -0,0 +1,58 @@
+package algorithms;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class AddTwoNumber {
+	
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode tmp = new ListNode(0), l3;
+		l3 = tmp;
+		int carry = 0;
+		while (true) {
+			if (l1.val + l2.val + carry > 9) {
+				tmp.val = l1.val + l2.val + carry - 10;
+				carry = 1;
+			} else {
+				tmp.val = l1.val + l2.val + carry;
+				carry = 0;
+			}
+			
+			if (l1.next != null || l2.next != null || carry != 0) {
+				tmp.next = new ListNode(0);
+				tmp = tmp.next;
+				
+				if (l1.next == null) l1.next = new ListNode(0);
+				
+				l1 = l1.next;
+				
+				if (l2.next == null) l2.next = new ListNode(0);
+				
+				l2 = l2.next;
+			} else {
+				break;
+			}
+		}
+        return l3;
+    }
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		ListNode l1 = new ListNode(6);
+		l1.next = new ListNode(0);
+		l1.next.next = new ListNode(3);
+		
+		ListNode l2 = new ListNode(3);
+		l2.next = new ListNode(0);
+		l2.next.next = new ListNode(8);
+		
+		ListNode l3 = addTwoNumbers(l1, l2);
+		System.out.print(l3.val);
+		System.out.print(l3.next.val);
+		System.out.print(l3.next.next.val);
+	}
+
+}
---
+++ src/algorithms/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,68 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	
+	public static int lengthOfLongestSubstring(String s) {
+//		first method:
+//		char[] chars = s.toCharArray();
+//		ArrayList<Character> repeat;
+//		int maxLen = 0, head = 0, c = 0;
+//		for (head = 0; head < chars.length; head++) {
+//			repeat = new ArrayList<Character>();
+//			for (c = head + 1; c < chars.length; c++) {
+//				if (chars[c] != chars[head]) {
+//					if (repeat.indexOf(chars[c]) > -1) {
+//						break;
+//					} else {
+//						repeat.add(chars[c]);
+//					}
+//				}
+//				
+//				if (chars[c] == chars[head]) break;
+//			}
+//			if (c - head > maxLen) maxLen = c - head;
+//		}
+//        return maxLen;
+
+//		second method:
+//		int maxLength=0;
+//        int indexOf=0;
+//        char[] chars = s.toCharArray();
+//
+//        for(int i=0;i<chars.length;i++){
+//            for(int j=indexOf;j<i;j++){
+//
+//                if(chars[j] == chars[i]){
+//                    maxLength=Math.max(maxLength,i-indexOf);
+//                    indexOf=j+1;
+//                    break;
+//                }
+//            }
+//        }
+//        return Math.max(maxLength,chars.length-indexOf);
+        
+//        third method:
+		int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.print(lengthOfLongestSubstring("xxzqi"));
+	}
+
+}
