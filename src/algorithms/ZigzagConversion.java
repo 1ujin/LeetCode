15db30565b07a237bc51991ba29094c72f0fa521
lujin <lujin.vip@foxmail.com>
2019-12-13 09:52:20 +0800

    Z字形变换修改注释和缩进字符

--- src/algorithms/ZigzagConversion.java
+++ src/algorithms/ZigzagConversion.java
@@ -4,28 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ZigzagConversion {
-	
-	// method 1
-	public static String convert1(String s, int numRows) {
-		if (numRows == 1) return s;
-		List<StringBuilder> rows = new ArrayList<StringBuilder>();
-		for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
-		for (int i = 0; i < s.length(); i++) {
-			int remainder = i % ((numRows - 1) * 2);
-			if (remainder < numRows) {
-				rows.get(remainder).append(s.charAt(i));
-			} else {
-				rows.get(2 * (numRows - 1) - remainder).append(s.charAt(i));
-			}
-		}
-		String ret = "";
+    
+    // method 1
+    public static String convert1(String s, int numRows) {
+        if (numRows == 1) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        for (int i = 0; i < s.length(); i++) {
+            int remainder = i % ((numRows - 1) * 2);
+            if (remainder < numRows) {
+                rows.get(remainder).append(s.charAt(i));
+            } else {
+                rows.get(2 * (numRows - 1) - remainder).append(s.charAt(i));
+            }
+        }
+        String ret = "";
         for (StringBuilder row : rows) ret += row.toString();
         return ret;
-	}
-	
-	// method 2 fastest
-	public static String convert2(String s, int numRows) {
-		if (numRows == 1) return s;
+    }
+    
+    // method 2 fastest
+    public static String convert2(String s, int numRows) {
+        if (numRows == 1) return s;
         int round = (numRows << 1) - 2;
         char[] arr = s.toCharArray();
         char[] res = new char[arr.length];
@@ -33,20 +33,22 @@ public class ZigzagConversion {
         for(int i = 0; i < numRows; i++) {
             for(int j = i; j < arr.length; j += round ) {
                 res[cur++] = arr[j];
+                // 当i不为第一行和最后一行时，要额外添加字符
+                // 并在后续遍历j时过滤掉这些已经添加的(j + round - i * 2 < arr.length)
                 if(i > 0 && i < numRows - 1 && j + round - i * 2 < arr.length) {
-                    res[cur++] = arr[j + round - i * 2 ];
+                    res[cur++] = arr[j + round - i * 2];
                 }
             }
         }
 
         return new String(res);
-	}
+    }
 
-	public static void main(String[] args) {
-		long startTime = System.nanoTime();
-		System.out.println(convert2("PAYPALISHIRING", 3));
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        System.out.println(convert2("PAYPALISHIRING", 5));
         long endTime = System.nanoTime();
         System.out.println("Duration: " + (endTime - startTime) + "ns");
-	}
+    }
 
 }
