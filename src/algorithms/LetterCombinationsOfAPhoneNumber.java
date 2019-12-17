87602b309e3087c5459c223314abf2adaab383ce
lujin <lujin.vip@foxmail.com>
2019-12-17 23:00:38 +0800

    电话号码的字母组合 (回溯)

--- src/algorithms/LetterCombinationsOfAPhoneNumber.java
+++ src/algorithms/LetterCombinationsOfAPhoneNumber.java
@@ -1,19 +1,35 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinationsOfAPhoneNumber {
-	
-	public static List<String> letterCombinations(String digits) {
-        return null;
+    
+    private static String[] values = new String[] {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    private static List<String> list = new ArrayList<String>();
+    
+    public static List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) return list;
+        int digit = (int) digits.charAt(0) - 48;
+        String letter = values[digit - 2], pop = "";
+        int i = 0, len = list.size();
+        do {
+            if (list.size() > 0) pop = list.remove(0);
+            for (int j = 0; j < letter.length(); j++) {
+                list.add(pop + letter.charAt(j));
+            }
+            i++;
+        } while (i < len);
+        return letterCombinations(digits.substring(1));
     }
 
-	public static void main(String[] args) {
-		long startTime = System.nanoTime();
-        System.out.println(letterCombinations("23"));
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        letterCombinations("234");
         long endTime = System.nanoTime();
         System.out.print("Duration: " + (endTime - startTime) + "ns");
 
-	}
+    }
 
 }
