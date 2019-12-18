d620c34b9ad0b103f2ff15451216dba99f355188
lujin <lujin.vip@foxmail.com>
2019-12-18 10:51:44 +0800

    无重复字符的最长子串

--- src/algorithms/LongestSubstringWithoutRepeatingCharacters.java
+++ src/algorithms/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,33 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters {
-	
-	// method 1
-	public static int lengthOfLongestSubstring1(String s) {
-		char[] chars = s.toCharArray();
-		ArrayList<Character> repeat;
-		int maxLen = 0, head = 0, c = 0;
-		for (head = 0; head < chars.length; head++) {
-			repeat = new ArrayList<Character>();
-			for (c = head + 1; c < chars.length; c++) {
-				if (chars[c] != chars[head]) {
-					if (repeat.indexOf(chars[c]) > -1) {
-						break;
-					} else {
-						repeat.add(chars[c]);
-					}
-				}
-				
-				if (chars[c] == chars[head]) break;
-			}
-			if (c - head > maxLen) maxLen = c - head;
-		}
-		return maxLen;
-	}
-	
-	// method 2
-	public static int lengthOfLongestSubstring2(String s) {
-		int maxLength=0;
+    
+    // method 1
+    public static int lengthOfLongestSubstring1(String s) {
+        char[] chars = s.toCharArray();
+        ArrayList<Character> repeat;
+        int maxLen = 0, head = 0, c = 0;
+        for (head = 0; head < chars.length; head++) {
+            repeat = new ArrayList<Character>();
+            for (c = head + 1; c < chars.length; c++) {
+                if (chars[c] != chars[head]) {
+                    if (repeat.indexOf(chars[c]) > -1) {
+                        break;
+                    } else {
+                        repeat.add(chars[c]);
+                    }
+                }
+                
+                if (chars[c] == chars[head]) break;
+            }
+            if (c - head > maxLen) maxLen = c - head;
+        }
+        return maxLen;
+    }
+    
+    // method 2
+    public static int lengthOfLongestSubstring2(String s) {
+        int maxLength=0;
         int indexOf=0;
         char[] chars = s.toCharArray();
 
@@ -46,11 +46,11 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
         }
         return Math.max(maxLength,chars.length-indexOf);
-	}
-	
-	// method 3
-	public static int lengthOfLongestSubstring3(String s) {
-		int n = s.length(), ans = 0;
+    }
+    
+    // method 3
+    public static int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), ans = 0;
         Map<Character, Integer> map = new HashMap<>(); // current index of character
         // try to extend the range [i, j]
         for (int j = 0, i = 0; j < n; j++) {
@@ -63,9 +63,11 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return ans;
     }
 
-	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-		System.out.print(lengthOfLongestSubstring3("xxzqi"));
-	}
+    public static void main(String[] args) {
+    	long startTime = System.nanoTime();
+        lengthOfLongestSubstring3("xxzqi");
+        long endTime = System.nanoTime();
+        System.out.print("Duration: " + (endTime - startTime) + "ns");
+    }
 
 }
