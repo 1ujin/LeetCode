70fbda094c2df441646f0fd8c3489d76684d6bc0
lujin <lujin.vip@foxmail.com>
2019-12-28 05:37:33 +0800

    两数相除

--- src/algorithms/DivideTwoInteger.java
+++ src/algorithms/DivideTwoInteger.java
@@ -6,37 +6,27 @@ public class DivideTwoInteger {
         if (dividend == 0) return 0;
         boolean positive = true;
         if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) positive = false;
-        if (dividend < 0) dividend = -dividend;
-        if (divisor < 0) divisor = -divisor;
-        int tmp = divisor, n = 0, quotient = 0;
-        StringBuffer sb = new StringBuffer();
-        while (dividend >= tmp) {
-            n++;
-            tmp <<= 1;
-        }
-        tmp = tmp >> 1;
-        for (int i = 0; i < n; i++) {
-            if (dividend >= tmp) {
-                sb.append(1);
-                quotient <<= 1;
-                if (quotient >= Integer.MAX_VALUE - 1) return Integer.MAX_VALUE;
-                quotient += 1;
+        if (dividend > 0) dividend = -dividend;
+        if (divisor > 0) divisor = -divisor;
+        int quotient = 0, tmp = divisor;
+        // 获取大于等于被除数的除数最小的二倍数tmp
+        while (dividend <= tmp << 1 && tmp << 1 < 0) tmp <<= 1;
+        while (tmp <= divisor && dividend <= 0) {
+            if (quotient << 1 == Integer.MIN_VALUE && positive) return Integer.MAX_VALUE;
+            quotient <<= 1;
+            if (dividend <= tmp) {
                 dividend -= tmp;
+                quotient += 1;
             }
-            else {
-                sb.append(0);
-                quotient <<= 1;
-            }
+            if (tmp == tmp >> 1) break;
             tmp >>= 1;
         }
-        System.out.println(sb.toString());
-        System.out.println(quotient);
         return positive ? quotient : -quotient;
     }
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        int quotient = divide(-2147483648, -1);
+        int quotient = divide(Integer.MIN_VALUE, -1);
         long endTime = System.nanoTime();
         System.out.println(quotient);
         System.out.print("Duration: " + (endTime - startTime) + "ns");
