c97bbb1ea95ee4b472d68adabf63837f6b5f14bb
LuJin <LuJin@LuJin-PC>
2019-12-30 11:08:23 +0800

    最长有效括号 (栈, 动态规划)

--- src/algorithms/LongestValidParentheses.java
+++ src/algorithms/LongestValidParentheses.java
@@ -4,11 +4,12 @@ import java.util.Stack;
 
 public class LongestValidParentheses {
     
-    public static int longestValidParentheses(String s) {
+    // method 1 stack
+    public static int longestValidParentheses1(String s) {
         if (s.length() == 0) return 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
-        int count = 0;
+        int maxLen = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 stack.push(i);
@@ -17,16 +18,33 @@ public class LongestValidParentheses {
                 if (stack.empty()) {
                     stack.push(i);
                 } else {
-                    count = Math.max(count, i - stack.peek());
+                    maxLen = Math.max(maxLen, i - stack.peek());
                 }
             }
         }
-        return count;
+        return maxLen;
+    }
+    
+    // method 2 dynamic programming
+    public static int longestValidParentheses2(String s) {
+        int maxLen = 0;
+        int dp[] = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+            }
+        }
+        return maxLen;
     }
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        int len = longestValidParentheses(")");
+        int len = longestValidParentheses2("()(()()");
         long endTime = System.nanoTime();
         System.out.println(len);
         System.out.print("Duration: " + (endTime - startTime) + "ns");
