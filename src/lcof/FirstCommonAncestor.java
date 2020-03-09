c2baf8b7095995b3b52902e65d179a8e7ae07c16
lujin <lujin.vip@foxmail.com>
2020-03-10 01:06:04 +0800

    面试题 04.08. 首个共同祖先 (深度优先搜索)

---
+++ src/lcof/FirstCommonAncestor.java
@@ -0,0 +1,26 @@
+package lcof;
+
+import util.Tree;
+import util.TreeNode;
+
+public class FirstCommonAncestor {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == q) return null;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null || root == p || root == q) return root;
+        if (left != null || right != null) return left != null ? left : right;
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new Tree(new int[] {3, 5, 1, 6, 2, 0, 8, -1, -1, 7, 4}).getRoot();
+        long startTime = System.nanoTime();
+        int result = new FirstCommonAncestor().lowestCommonAncestor(root, root.left.right, root.right).val;
+        long endTime = System.nanoTime();
+        System.out.println(result);
+        System.out.print("Duration: " + (endTime - startTime) + "ns");
+    }
+
+}
