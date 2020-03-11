16c8821cd8dde8ae44629ad548fc70c1bfecd881
lujin <lujin.vip@foxmail.com>
2020-03-12 06:28:52 +0800

    字符串的最大公因子

---
+++ src/algorithms/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class GreatestCommonDivisorOfStrings {
+    
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+    
+    private int gcd(int a, int b) {
+        return b == 0? a: gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        String result = new GreatestCommonDivisorOfStrings().gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX");
+        long endTime = System.nanoTime();
+        System.out.println(result);
+        System.out.print("Duration: " + (endTime - startTime) + "ns");
+    }
+
+}
