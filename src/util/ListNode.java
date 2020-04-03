082dac93922efd59fe764e16709ef96e4d059072
lujin <lujin.vip@foxmail.com>
2020-04-03 20:44:24 +0800

    链表节点

--- src/util/ListNode.java
+++ src/util/ListNode.java
@@ -4,4 +4,19 @@ public class ListNode {
     public int val;
     public ListNode next;
     public ListNode(int x) { val = x; }
+    
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+    
+    public static String toLinkedListString(ListNode node) {
+        StringBuilder s = new StringBuilder(node.val);
+        while (node.next != null) {
+            node = node.next;
+            s.append(" -> ");
+            s.append(node.val);
+        }
+        return s.toString();
+    }
 }
