be1ee5ea70e40aad193c48aa5b1513751de6552c
lujin <lujin.vip@foxmail.com>
2020-04-03 16:39:27 +0800

    二叉树节点

--- src/util/TreeNode.java
+++ src/util/TreeNode.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -9,5 +13,22 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+    
+    @Override
+    public String toString() {
+        List<TreeNode> list = new ArrayList<>();
+        list.add(this);
+        for (int i = 0; i < list.size(); i++) {
+            TreeNode t = list.get(i);
+            if (t == null || (t.left == null && t.right == null))
+                continue;
+            list.add(t.left);
+            list.add(t.right);
+        }
+        if (list.get(list.size() - 1) == null)
+            list.remove(list.size() - 1);
+        return list.stream().map(n -> n == null ? null : n.val)
+                .collect(Collectors.toList()).toString();
     }
 }
