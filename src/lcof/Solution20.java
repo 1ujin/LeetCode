55e9071e231e037e155e4afe7240d6a2fc2aa092
lujin <lujin.vip@foxmail.com>
2020-04-17 17:00:21 +0800

    面试题20. 表示数值的字符串 (状态机)

---
+++ src/lcof/Solution20.java
@@ -0,0 +1,38 @@
+package lcof;
+
+public class Solution20 {
+    
+    public boolean isNumber(String s) {
+        int state = 0, finals = 0b101101000;
+        int[][] matrix = { 
+                {  0,  1,  6,  2, -1 },
+                { -1, -1,  6,  2, -1 },
+                { -1, -1,  3, -1, -1 },
+                {  8, -1,  3, -1,  4 },
+                { -1,  7,  5, -1, -1 },
+                {  8, -1,  5, -1, -1 },
+                {  8, -1,  6,  3,  4 },
+                { -1, -1,  5, -1, -1 },
+                {  8, -1, -1, -1, -1 } };
+        for (char c : s.toCharArray()) {
+            int id = -1;
+            switch (c) {
+                case ' ': id = 0; break;
+                case '+':
+                case '-': id = 1; break;
+                case '.': id = 3; break;
+                case 'e': id = 4; break;
+                default: if (c > 47 && c < 58) id = 2;
+            }
+            if (id < 0) return false;
+            state = matrix[state][id];
+            if (state < 0) return false;
+        }
+        return (finals & 1 << state) > 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution20().isNumber("+100"));
+    }
+
+}
