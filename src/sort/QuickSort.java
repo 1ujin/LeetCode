4c75c5d812af91b282a0cf59d12e2056e9bcbd74
lujin <lujin.vip@foxmail.com>
2020-04-19 20:46:16 +0800

    快速排序

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,5 +1,16 @@
 package sort;
 
+/**
+ * <p>平均时间复杂度: O(nlogn)
+ * <p>最好情况: O(nlogn)
+ * <p>最坏情况: O(n^2)
+ * <p>额外空间复杂度: O(logn)
+ * <p>排序方式: In-place
+ * <p>稳定性: 不稳定
+ * <p>描述: (小数, 基准元素, 大数)。
+ * 在区间中间挑选一个元素作基准, 将小于基准的元素放在基准之前, 
+ * 大于基准的元素放在基准之后, 再分别对小数区与大数区进行排序(分治)。
+ */
 public class QuickSort {
 
     public static void quickSort(int[] a) {
