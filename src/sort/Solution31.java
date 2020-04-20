854964b74eecf8ca7e603947702cf2a24ecbdda0
lujin <lujin.vip@foxmail.com>
2020-04-21 04:55:24 +0800

    面试题31. 栈的压入、弹出序列 (栈)

---
+++ src/sort/Solution31.java
@@ -0,0 +1,30 @@
+package sort;
+
+import java.util.Stack;
+
+public class Solution31 {
+    
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int inLen = pushed.length, outLen =  popped.length;
+        if (inLen == 0 && outLen == 0) return true;
+        int ip = 0, op = 0;
+        Stack<Integer> stack = new Stack<>();
+        while (ip < inLen || stack.peek().equals(popped[op])) {
+            if (ip < inLen)
+                stack.push(pushed[ip++]);
+            while (!stack.isEmpty() && stack.peek().equals(popped[op])) {
+                stack.pop();
+                op++;
+            }
+            if (op == outLen) return true;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        int[] pushed = { 1, 2, 3, 4, 5 };
+        int[] popped = { 4, 3, 5, 2, 1 };
+        System.out.println(new Solution31().validateStackSequences(pushed, popped));
+    }
+
+}
