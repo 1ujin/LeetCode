bc85fb5b61c4d9de5bdb755c1c77de5e6298b0c6
lujin <lujin.vip@foxmail.com>
2020-04-24 04:16:40 +0800

    138. 复制带随机指针的链表

---
+++ src/algorithms/CopyListWithRandomPointer.java
@@ -0,0 +1,49 @@
+package algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+public class CopyListWithRandomPointer {
+    
+    Map<Node, Node> map = new HashMap<>();
+    
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        if (map.containsKey(head)) return map.get(head);
+        Node newHead = new Node(head.val);
+        map.put(head, newHead);
+        newHead.next = copyRandomList(head.next);
+        newHead.random = copyRandomList(head.random);
+        return newHead;
+    }
+    
+    public static void main(String[] args) {
+        Node head = new Node(7);
+        Node node1 = new Node(13);
+        Node node2 = new Node(11);
+        Node node3 = new Node(10);
+        Node node4 = new Node(1);
+        head.next = node1;
+        node1.next = node2;
+        node1.random = head;
+        node2.next = node3;
+        node2.random = node4;
+        node3.next = node4;
+        node3.random = node2;
+        node4.random = head;
+        new CopyListWithRandomPointer().copyRandomList(head);
+    }
+
+}
