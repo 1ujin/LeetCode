bb5967edd1f67de0be58ba99c47b1ed6ed2d1515
lujin <lujin.vip@foxmail.com>
2020-05-08 02:36:22 +0800

    面试题68 - II. 二叉树的最近公共祖先 (深度优先搜索)

---
+++ src/lcof/Solution68II.java
@@ -0,0 +1,25 @@
+package lcof;
+
+import util.Tree;
+import util.TreeNode;
+
+public class Solution68II {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int[] vals = { 3, 5, 1, 6, 2, 0, 8, Integer.MIN_VALUE,
+                Integer.MIN_VALUE, 7, 4 };
+        TreeNode root = new Tree(vals).getRoot();
+        System.out.println(new Solution68II().lowestCommonAncestor(root,
+                root.left, root.left.right.right));
+    }
+
+}
