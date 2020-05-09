f6c63ff234359e709e2298c8970d78f393e89fda
LuJin <LuJin@LuJin-PC>
2020-05-09 15:54:18 +0800

    57. 插入区间

---
+++ src/algorithms/InsertIntervals.java
@@ -0,0 +1,37 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class InsertIntervals {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int len = intervals.length, lo = 0, hi = len - 1, left = 0, right = 0;
+        if (len == 0) return new int[][] { newInterval };
+        while (lo < len && intervals[lo][1] < newInterval[0]) lo++;
+        if (lo == len) {
+            int[][] newIntervals = new int[intervals.length + 1][2];
+            System.arraycopy(intervals, 0, newIntervals, 0, len);
+            newIntervals[len] = newInterval;
+            return newIntervals;
+        } else left = intervals[lo][0] < newInterval[0] ? intervals[lo][0] : newInterval[0];
+        while (hi > -1 && intervals[hi][0] > newInterval[1]) hi--;
+        if (hi == -1) {
+            int[][] newIntervals = new int[intervals.length + 1][2];
+            System.arraycopy(intervals, 1, newIntervals, 1, len);
+            newIntervals[0] = newInterval;
+            return newIntervals;
+        } else right = intervals[hi][1] > newInterval[1] ? intervals[hi][1] : newInterval[1];
+        int[][] newIntervals = new int[len - hi + lo][2];
+        System.arraycopy(intervals, 0, newIntervals, 0, lo);
+        newIntervals[lo] = new int[] { left, right };
+        System.arraycopy(intervals, hi + 1, newIntervals, lo + 1, len - hi - 1);
+        return newIntervals;
+    }
+    
+    public static void main(String[] args) {
+        int[][] intervals = { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } };
+        int[] newInterval = { 4, 8 };
+        System.out.println(Arrays.deepToString(new InsertIntervals().insert(intervals, newInterval)));
+    }
+
+}
