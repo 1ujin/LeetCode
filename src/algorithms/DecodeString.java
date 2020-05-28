472c124da6e629abe39ec0cb08b016f1f3de8bfc
lujin <lujin@lujin-PC>
2020-05-28 12:20:13 +0800

    394. 字符串解码 (栈)

--- src/algorithms/DecodeString.java
+++ src/algorithms/DecodeString.java
@@ -1,28 +1,28 @@
 package algorithms;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
 public class DecodeString {
     
     public String decodeString(String s) {
+        int count = 0;
         StringBuilder sb = new StringBuilder();
-        int multi = 0;
-        LinkedList<Integer> multiStack = new LinkedList<>();
-        LinkedList<String> sbStack = new LinkedList<>();
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> sbStack = new Stack<>();
         for (Character c : s.toCharArray()) {
             if (c == '[') {
-                multiStack.addLast(multi);
-                sbStack.addLast(sb.toString());
+                countStack.push(count);
+                sbStack.push(sb);
                 sb = new StringBuilder();
-                multi = 0;
+                count = 0;
             } else if (c == ']') {
                 StringBuilder tmp = new StringBuilder();
-                int lastMulti = multiStack.removeLast();
-                for (int i = 0; i < lastMulti; i++)
+                int lastCount = countStack.pop();
+                for (int i = 0; i < lastCount; i++)
                     tmp.append(sb);
-                sb = new StringBuilder(sbStack.removeLast() + tmp);
+                sb = sbStack.pop().append(tmp);
             } else if (c >= '0' && c <= '9')
-                multi = multi * 10 + Integer.parseInt(c + "");
+                count = count * 10 + c - '0';
             else sb.append(c);
         }
         return sb.toString();
