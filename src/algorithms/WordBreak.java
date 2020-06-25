f2780b19043215aa4fd7b2395fe70606eaac1d67
lujin <lujin.vip@foxmail.com>
2020-06-26 00:23:13 +0800

    139. 单词拆分 (动态规划）

---
+++ src/algorithms/WordBreak.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[len];
+    }
+
+    public static void main(String[] args) {
+        List<String> wordDict = Arrays.asList(new String[] { "leet", "de" });
+        System.out.println(new WordBreak().wordBreak("leetcode", wordDict));
+    }
+
+}
