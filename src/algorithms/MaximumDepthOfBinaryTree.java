ad28f945617053d9590bb4b9df99ac1b6f413bee
lujin <lujin.vip@foxmail.com>
2020-07-28 01:57:13 +0800

    104. 二叉树的最大深度 (深度优先搜索)

---
+++ src/algorithms/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import util.Tree;
+import util.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] vals = { 3, 9, 20, Integer.MIN_VALUE, Integer.MIN_VALUE, 15, 7 };
+        TreeNode root = new Tree(vals).getRoot();
+        System.out.println(new MaximumDepthOfBinaryTree().maxDepth(root));
+    }
+
+}
