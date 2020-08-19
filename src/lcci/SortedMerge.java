e19d4267c3d9148b4d863a1db00762a632b4c958
lujin <lujin.vip@foxmail.com>
2020-08-19 15:59:51 +0800

    面试题 10.01. 合并排序的数组 (双指针)

--- src/lcci/SortedMerge.java
+++ src/lcci/SortedMerge.java
@@ -1,22 +1,23 @@
 package lcci;
 
+import java.util.Arrays;
+
 public class SortedMerge {
-    
+
     public void merge(int[] A, int m, int[] B, int n) {
         while (m > 0 && n > 0)
-            A[m + n - 1] = A[m - 1] > B[n - 1] ? A[m-- - 1] : B[n-- - 1];
+            A[m + n - 1] = A[m - 1] > B[n - 1] ? A[--m] : B[--n];
         while (n > 0)
-            A[n - 1] = B[n-- - 1];
+            A[n - 1] = B[--n];
     }
 
     public static void main(String[] args) {
-        int[] A = new int[] {1, 2, 3, 0, 0, 0}, B = new int[] {2, 5, 6};
+        int[] A = { 1, 2, 3, 0, 0, 0 }, B = { 2, 5, 6 };
         long startTime = System.nanoTime();
         new SortedMerge().merge(A, 3, B, 3);
         long endTime = System.nanoTime();
-        for (int i = 0; i < A.length; i++)
-            System.out.print(A[i] + ",");
-        System.out.print("\nDuration: " + (endTime - startTime) + "ns");
+        System.out.println(Arrays.toString(A));
+        System.out.print("Duration: " + (endTime - startTime) + "ns");
     }
 
 }
