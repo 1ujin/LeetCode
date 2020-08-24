3eb7e2b50a015cb36d8fb125a28f43b1d16e3bf1
lujin <lujin.vip@foxmail.com>
2020-08-25 05:00:05 +0800

    491. 递增子序列 (深度优先搜索)

---
+++ src/algorithms/IncreasingSubsequences.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IncreasingSubsequences {
+    
+    int[] nums;
+    List<Integer> temp = new ArrayList<>();
+    List<List<Integer>> subseqs = new ArrayList<>();
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        this.nums = nums;
+        dfs(0, Integer.MIN_VALUE);
+        return subseqs;
+    }
+
+    private void dfs(int index, int last) {
+        if (index == nums.length) {
+            if (temp.size() >= 2)
+                subseqs.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        if (nums[index] >= last) {
+            temp.add(nums[index]);
+            dfs(index + 1, nums[index]);
+            temp.remove(temp.size() - 1);
+        }
+        if (nums[index] != last)
+            dfs(index + 1, last);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 1, 1, 1, 2 };
+        System.out.println(new IncreasingSubsequences().findSubsequences(nums));
+    }
+
+}
