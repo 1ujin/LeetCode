858a5c93f8ddc0ca269a501a667183ab559522cf
lujin <lujin.vip@foxmail.com>
2020-09-21 02:32:33 +0800

    538. 把二叉搜索树转换为累加树 (深度优先搜索)

--- src/algorithms/ConvertBstToGreaterTree.java
+++ src/algorithms/ConvertBstToGreaterTree.java
@@ -5,21 +5,36 @@ import util.TreeNode;
 
 public class ConvertBstToGreaterTree {
 
-    public TreeNode convertBST(TreeNode root) {
-        dfs(root, 0);
+    // method 1
+    public TreeNode convertBST1(TreeNode root) {
+        dfs1(root, 0);
         return root;
     }
 
-    private int dfs(TreeNode root, int sum) {
+    private int dfs1(TreeNode root, int sum) {
         if (root == null) return sum;
-        root.val += root.right == null ? sum : dfs(root.right, sum);
-        return root.left == null ? root.val : dfs(root.left, root.val);
+        root.val += root.right == null ? sum : dfs1(root.right, sum);
+        return root.left == null ? root.val : dfs1(root.left, root.val);
+    }
+    
+    // method 2
+    int sum = 0;
+    
+    public TreeNode convertBST2(TreeNode root) {
+        dfs2(root);
+        return root;
+    }
+    
+    private TreeNode dfs2(TreeNode root) {
+        if (root == null) return null;
+        sum = root.val += root.right == null ? sum : dfs2(root.right).val;
+        return root.left == null ? root : dfs2(root.left);
     }
 
     public static void main(String[] args) {
         int[] vals = { 2, 0, 3, -4, 1 };
         TreeNode root = new Tree(vals).getRoot();
-        new ConvertBstToGreaterTree().convertBST(root);
+        new ConvertBstToGreaterTree().convertBST2(root);
         System.out.println(TreeNode.toTreeString(root));
     }
 
