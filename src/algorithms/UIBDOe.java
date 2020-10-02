1d6653186e55708b2cccba8df38bfc5b639efb89
lujin <lujin.vip@foxmail.com>
2020-10-02 18:39:25 +0800

    LCP 19. 秋叶收藏集 (动态规划, 状态机)

---
+++ src/algorithms/UIBDOe.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+public class UIBDOe {
+    
+    public int minimumOperations(String leaves) {
+        char[] cs = leaves.toCharArray();
+        int len = cs.length;
+        int[][] dp = new int[len][3];
+        dp[0][0] = cs[0] == 'y' ? 1 : 0;
+        dp[0][1] = dp[0][2] = dp[1][2] = Integer.MAX_VALUE;
+        for (int i = 1; i < len; i++) {
+            int isYellow = cs[i] == 'y' ? 1 : 0;
+            dp[i][0] = dp[i - 1][0] + isYellow;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]) + (isYellow ^ 1);
+            if (i >= 2)
+                dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + isYellow;
+        }
+        return dp[len - 1][2];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new UIBDOe().minimumOperations("rrryyyrryyyrr"));
+    }
+
+}
