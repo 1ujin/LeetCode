d63ca4d82aa6ce651c3472ac94be81bd07b6abb0
lujin <lujin.vip@foxmail.com>
2020-11-08 02:50:47 +0800

    122. 买卖股票的最佳时机 II (贪心算法, 动态规划)

---
+++ src/algorithms/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+public class BestTimeToBuyAndSellStock2 {
+
+    // method 1 greedy algorithm fastest
+    public int maxProfit1(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++)
+            if (prices[i] > prices[i - 1])
+                profit += prices[i] - prices[i - 1];
+        return profit;
+    }
+    
+    // method 2 dynamic programming
+    public int maxProfit2(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return Math.max(dp[len - 1][0], dp[len - 1][0]);
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(new BestTimeToBuyAndSellStock2().maxProfit1(prices));
+    }
+
+}
