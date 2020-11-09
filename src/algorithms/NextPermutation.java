d681215d50973d9178153d074c2e9da46b24b2a9
lujin <lujin.vip@foxmail.com>
2020-11-10 04:18:21 +0800

    31. 下一个排列

--- src/algorithms/NextPermutation.java
+++ src/algorithms/NextPermutation.java
@@ -4,28 +4,27 @@ import java.util.Arrays;
 
 public class NextPermutation {
     
-    public static void nextPermutation(int[] nums) {
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                Arrays.sort(nums);
-            } else if (nums[i] > nums[i - 1]) {
-                int tmpIndex = i;
-                for (int j = i; j < nums.length; j++) {
-                    tmpIndex = nums[tmpIndex] > nums[j] && nums[j] > nums[i - 1] ? j : tmpIndex;
-                }
-                int tmp = nums[tmpIndex];
-                nums[tmpIndex] = nums[i - 1];
-                nums[i - 1] = tmp;
-                Arrays.sort(nums, i, nums.length);
-                break;
-            }
+    public void nextPermutation(int[] nums) {
+        int len = nums.length, i = len - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if (i > -1) {
+            int j = len - 1;
+            while (j > 0 && nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
         }
+        while (++i < len--) swap(nums, i, len);
+    }
+
+    private void swap (int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[] {3, 2, 1};
+        int[] nums = new int[] { 3, 2, 1 };
         long startTime = System.nanoTime();
-        nextPermutation(nums);
+        new NextPermutation().nextPermutation(nums);
         long endTime = System.nanoTime();
         System.out.println(Arrays.toString(nums));
         System.out.print("Duration: " + (endTime - startTime) + "ns");
