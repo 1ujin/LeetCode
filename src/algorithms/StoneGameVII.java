1fa8a9cd2856f8f87143ac1f35f450e035a12476
lujin <lujin.vip@foxmail.com>
2020-12-14 04:42:59 +0800

    5627. 石子游戏 VII (动态规划)

--- src/algorithms/StoneGameVII.java
+++ src/algorithms/StoneGameVII.java
@@ -8,12 +8,12 @@ public class StoneGameVII {
         for (int i = 2; i <= len; i++) {
             for (int j = 0; j <= len - i; j++) {
                 if (i == 2) {
-                    dp[i][i + 1] = Math.max(stones[i], stones[i + 1]);
-                    sum[i][i + 1] = stones[i] + stones[i + 1];
+                    dp[j][i + j - 1] = Math.max(stones[j], stones[i + j - 1]);
+                    sum[j][i + j - 1] = stones[j] + stones[i + j - 1];
                 } else {
                     dp[j][i + j - 1] = Math.max(
                             sum[j][i + j - 2] - dp[j][i + j - 2],
-                            sum[j][i + j - 1] - dp[j][i + j - 1]);
+                            sum[j + 1][i + j - 1] - dp[j + 1][i + j - 1]);
                     sum[j][i + j - 1] = stones[j] + sum[j + 1][i + j - 1];
                 }
             }
