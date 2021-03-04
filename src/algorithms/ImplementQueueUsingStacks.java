e7ed1396fddbe9d35d4bf8252520566e8df77c01
lujin <lujin.vip@foxmail.com>
2021-03-05 01:41:46 +0800

    232. 用栈实现队列

---
+++ src/algorithms/ImplementQueueUsingStacks.java
@@ -0,0 +1,52 @@
+package algorithms;
+
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> stack1, stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while (!empty())
+            stack2.push(stack1.pop());
+        stack1.push(x);
+        while (!stack2.isEmpty())
+            stack1.push(stack2.pop());
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack1.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return stack1.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+
+}
+
+public class ImplementQueueUsingStacks {
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.push(2);
+        System.out.println(myQueue.peek());
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.empty());
+    }
+
+}
