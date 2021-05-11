92850d24c1b1bee4be8b301009795b80318528c3
lujin <lujin.vip@foxmail.com>
2021-05-12 05:01:41 +0800

    1310. 子数组异或查询 (位运算, 动态规划)

---
+++ src/algorithms/XorQueriesOfASubarray.java
@@ -0,0 +1,23 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class XorQueriesOfASubarray {
+
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] dp = new int[arr.length + 1];
+        for (int i = 1; i <= arr.length; i++)
+            dp[i] ^= dp[i - 1] ^ arr[i - 1];
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++)
+            result[i] = dp[queries[i][0]] ^ dp[queries[i][1] + 1];
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 4, 8 };
+        int[][] queries = { { 0, 1 }, { 1, 2 }, { 0, 3 }, { 3, 3 } };
+        System.out.println(Arrays.toString(new XorQueriesOfASubarray().xorQueries(arr, queries)));
+    }
+
+}
