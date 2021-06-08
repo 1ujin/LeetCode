28e0f8161c3c6446dd764fc83972e172194c1a6b
lujin <lujin.vip@foxmail.com>
2021-06-09 00:15:07 +0800

    879. 盈利计划 (动态规划)

---
+++ src/algorithms/ProfitableSchemes.java
@@ -0,0 +1,27 @@
+package algorithms;
+
+public class ProfitableSchemes {
+
+    public int profitableSchemes(int n, int minProfit, int[] group,
+            int[] profit) {
+        int[][] dp = new int[n + 1][minProfit + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+        int len = group.length, MOD = (int) 1e9 + 7;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = n; j >= members; j--)
+                for (int k = minProfit; k >= 0; k--)
+                    dp[j][k] = (dp[j][k] + dp[j - members][Math.max(0, k - earn)]) % MOD;
+        }
+        return dp[n][minProfit];
+    }
+
+    public static void main(String[] args) {
+        int[] group = { 2, 3, 5 };
+        int[] profit = { 6, 7, 8 };
+        System.out.println(new ProfitableSchemes().profitableSchemes(10, 5,
+                group, profit));
+    }
+
+}
