26b99ccd3fcf05a76290f2b8009b943ed7c422a8
lujin <lujin.vip@foxmail.com>
2021-07-11 03:54:37 +0800

    274. H 指数

---
+++ src/algorithms/HIndex.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class HIndex {
+    
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int min = citations[0], max = citations[citations.length - 1], h = 0;
+        int[] counts = new int[max - min + 2];
+        for (int citation : citations)
+            counts[citation - min]++;
+        for (int i = max - min; i >= 0; i--)
+            if ((counts[i] += counts[i + 1]) > h && min + i > h)
+                h = Math.min(counts[i], min + i);
+        return h;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = {3,0,6,1,5};
+        System.out.println(new HIndex().hIndex(citations));
+    }
+
+}
