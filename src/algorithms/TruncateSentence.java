a1bb2d977431ffbe3d0912efad9a17fc7d341cf4
lujin <lujin.vip@foxmail.com>
2021-12-06 01:04:08 +0800

    1816. 截断句子

---
+++ src/algorithms/TruncateSentence.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class TruncateSentence {
+
+    public String truncateSentence(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ')
+                k--;
+            if (k <= 0)
+                break;
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TruncateSentence()
+                .truncateSentence("Hello how are you Contestant", 4));
+    }
+
+}
