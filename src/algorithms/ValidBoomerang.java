5e0aaf96e3148e9585ac6b24dd6fdccf7398751e
lujin <lujin.vip@foxmail.com>
2022-06-08 02:11:47 +0800

    1037. 有效的回旋镖

--- src/algorithms/ValidBoomerang.java
+++ src/algorithms/ValidBoomerang.java
@@ -3,6 +3,7 @@ package algorithms;
 public class ValidBoomerang {
 
     public boolean isBoomerang(int[][] points) {
+        // 向量叉乘
         int[] v1 = { points[0][0] - points[1][0], points[0][1] - points[1][1] };
         int[] v2 = { points[0][0] - points[2][0], points[0][1] - points[2][1] };
         return v1[0] * v2[1] - v1[1] * v2[0] != 0;
